f289c850127c9c7a6cc0b53fb49af7f3fc0b04ea
Raul ALejandro Chauvin Ojeda <raul.chauvin@telefonica.com>
2019-03-28 21:33:49 -0500

    Correccion en lectura de caso de corrupcion

--- resources/assets/frontend/js/pages/corruption-cases.js
+++ resources/assets/frontend/js/pages/corruption-cases.js
@@ -8,31 +8,29 @@ function loadCorruptionCase(caseDetailUrl){
         },
         success: function (data) {
           	if(data.status === true){
-          		
           		var oCorruptionCase = data.oCorruptionCase;
-				$('#corruptionCaseImg').html('<img class=\'d-block w-100 h-100\' src=\''+oCorruptionCase.main_multimedia+'\' alt=\''+oCorruptionCase.title+'\'>');
-				$('#corruptionCaseTitle').html(oCorruptionCase.title);
-				$('#corruptionCaseSummary').html(oCorruptionCase.summary);
-				$('#corruptionCaseCaseStage').html(oCorruptionCase.case_stage);
-				$('#corruptionCaseCaseStageDetail').html(oCorruptionCase.case_stage_detail);
-				$('#corruptionCaseInvolvedNumber').html(oCorruptionCase.involved_number + ' personas');
-				$('#corruptionCasePeriod').html(oCorruptionCase.period);
-				$('#corruptionCaseStateFunction').html(oCorruptionCase.state_function);
-				$('#corruptionCaseProvince').html(oCorruptionCase.province);
-				$('#corruptionCaseUrl').html('<a href=\''+oCorruptionCase.url+'\' role=\'button\' class=\'btn btn-info btn-sm\'>¿Quieres conocer más?</a>');
+				      $('#corruptionCaseImg').html('<img class=\'d-block w-100 h-100\' src=\''+oCorruptionCase.main_multimedia+'\' alt=\''+oCorruptionCase.title+'\'>');
+				      $('#corruptionCaseTitle').html(oCorruptionCase.title);
+				      $('#corruptionCaseSummary').html(oCorruptionCase.summary);
+				      $('#corruptionCaseCaseStage').html(oCorruptionCase.case_stage);
+				      $('#corruptionCaseCaseStageDetail').html(oCorruptionCase.case_stage_detail);
+				      $('#corruptionCaseInvolvedNumber').html(oCorruptionCase.involved_number + ' personas');
+				      $('#corruptionCasePeriod').html(oCorruptionCase.period);
+				      $('#corruptionCaseStateFunction').html(oCorruptionCase.state_function);
+				      $('#corruptionCaseProvince').html(oCorruptionCase.province);
+				      $('#corruptionCaseUrl').html('<a href=\''+oCorruptionCase.url+'\' role=\'button\' class=\'btn btn-info btn-sm\'>¿Quieres conocer más?</a>');
 				
-				var linkedInstitutionsCmb = $('#corruptionCaseLinkedInstitutions');
-				linkedInstitutionsCmb.empty();
-				$.each(oCorruptionCase.linked_institutions, function(index, linkedInstitution){
-					linkedInstitutionsCmb.append('<option>'+linkedInstitution+'</option>');
-				});
+				      var linkedInstitutionsCmb = $('#corruptionCaseLinkedInstitutions');
+				      linkedInstitutionsCmb.empty();
+				      $.each(oCorruptionCase.linked_institutions, function(index, linkedInstitution){
+                linkedInstitutionsCmb.append('<option>'+linkedInstitution+'</option>');
+				      });
 
-				var publicOfficialsInvolvedCmb = $('#corruptionCasePublicOfficialsInvolved');
-				publicOfficialsInvolvedCmb.empty();
-				$.each(oCorruptionCase.public_officials_involved, function(index, publicOfficialInvolved){
-					publicOfficialsInvolvedCmb.append('<option>'+publicOfficialInvolved+'</option>');
-				});
-
+				      var publicOfficialsInvolvedCmb = $('#corruptionCasePublicOfficialsInvolved');
+				      publicOfficialsInvolvedCmb.empty();
+				      $.each(oCorruptionCase.public_officials_involved, function(index, publicOfficialInvolved){
+                publicOfficialsInvolvedCmb.append('<option>'+publicOfficialInvolved+'</option>');
+				      });
           	}else{
             	swal({
 	            	title: 'Casos de Corrupción',
@@ -47,7 +45,7 @@ function loadCorruptionCase(caseDetailUrl){
 	              	closeOnCancel: true,
 	              	confirmButtonText: 'OK',
 	              	cancelButtonText: '',
-	            }
+	            });
           	}
         },
         error: function(errors){
@@ -64,8 +62,7 @@ function loadCorruptionCase(caseDetailUrl){
               	closeOnCancel: true,
               	confirmButtonText: 'OK',
               	cancelButtonText: '',
-            }
-        );
+            });
         }
     });
 }
