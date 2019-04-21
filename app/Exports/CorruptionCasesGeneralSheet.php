1b9b0960478eb9e26c27bbc1ad9c95a2c9a3a07f
Pablo Reyes <yellowgamepjrr@gmail.com>
2019-04-21 16:15:37 -0500

    mejoras excel

--- app/Exports/CorruptionCasesGeneralSheet.php
+++ app/Exports/CorruptionCasesGeneralSheet.php
@@ -9,7 +9,7 @@ use Maatwebsite\Excel\Concerns\WithTitle;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithEvents;
 
-class CorruptionCasesGeneralSheet implements  WithTitle, ShouldAutoSize, WithEvents
+class CorruptionCasesGeneralSheet implements FromView, WithTitle, ShouldAutoSize, WithEvents
 {
 
     /**
@@ -20,24 +20,38 @@ class CorruptionCasesGeneralSheet implements  WithTitle, ShouldAutoSize, WithEve
         return 'Datos Generales';
     }
 
-    /**
-     * @return array
-     */
     public function registerEvents(): array
     {
+
+        $styleArray = [
+            'font' => [
+                'bold' => true;
+            ]
+        ];
+
         return [
+            AfterSheet::class => function(AfterSheet $event) use ($styleArray) {
+                $event->sheet->getStyle('A1:G1')->applyFromArray($styleArray);
+            },
             AfterSheet::class => function(AfterSheet $event) {
                 $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
-                $drawing->setName('Logo');
-                $drawing->setDescription('Logo');
-                $drawing->setPath('http://www.observatorioanticorrupcion.ec/public/frontend/images/logo-sitio.png');
+                $drawing->setName('Fundación Ciudadanía y Desarrollo');
+                $drawing->setDescription('Fundación Ciudadanía y Desarrollo');
+                $drawing->setPath(public_path('frontend/images/fcd.png'));
+                $drawing->setHeight(200);
                 $drawing->setCoordinates('D1');
 
                 $drawing->setWorksheet($event->sheet->getDelegate());
             },
         ];
     }
-    
+
+    public function view(): View
+    {
+        return view('frontend.exports.corruption-cases', [
+            'corruptionCases' => CorruptionCase::all()
+        ]);
+    }
 
 
 
