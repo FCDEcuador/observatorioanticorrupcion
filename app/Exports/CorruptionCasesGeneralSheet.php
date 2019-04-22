e8c72bf4bd6fe666a5f7942d8464f423cf421471
Pablo Reyes <yellowgamepjrr@gmail.com>
2019-04-21 19:58:34 -0500

    mejoras excel

--- app/Exports/CorruptionCasesGeneralSheet.php
+++ app/Exports/CorruptionCasesGeneralSheet.php
@@ -37,33 +37,45 @@ class CorruptionCasesGeneralSheet implements FromView, WithTitle, ShouldAutoSize
         $styleArray = [
             'font' => [
                 'bold' => true,
+                'size' => '16px',
             ],
             'horizontal' => \PhpOffice\PhpSpreadsheet\Style\Alignment::HORIZONTAL_CENTER, 
             'vertical' => \PhpOffice\PhpSpreadsheet\Style\Alignment::VERTICAL_CENTER,
+        ];
+
+        $styleArray2 = [
+            'borders' => [
+                'outline' => [
+                    'borderStyle' => \PhpOffice\PhpSpreadsheet\Style\Border::BORDER_THICK,
+                    'color' => ['argb' => 'FFFF0000'],
+                ],
+            ],
         ];
 
         return [
             AfterSheet::class => function(AfterSheet $event) use ($styleArray) {
                 
                 $event->sheet->getStyle('A9')->applyFromArray($styleArray);
+
+                $event->sheet->getStyle('A11:F13')->applyFromArray($styleArray2);
                 
-                $drawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
-                $drawing->setName('Logo');
-                $drawing->setDescription('Logo');
-                $drawing->setPath(public_path('frontend/images/logo-sitio.png'));
-                $drawing->setHeight(100);
-                $drawing->setWidth(200);
-                $drawing->setCoordinates('C2');
+                $objDrawing = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+                $objDrawing->setName('Logo');
+                $objDrawing->setDescription('Logo');
+                $objDrawing->setPath(public_path('frontend/images/logo-sitio.png'));
+                $objDrawing->setWidthAndHeight(148,74);
+                $objobjDrawing->setResizeProportional(true);
+                $objDrawing->setCoordinates('C2');
 
-                $drawing->setWorksheet($event->sheet->getDelegate());
+                $objDrawing->setWorksheet($event->sheet->getDelegate());
 
-                $drawing2 = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
-                $drawing2->setName('FCD');
-                $drawing2->setDescription('FCD');
-                $drawing2->setPath(public_path('frontend/images/fcd.png'));
-                $drawing2->setHeight(100);
-                $drawing2->setWidth(100);
-                $drawing2->setCoordinates('H2');
+                $objDrawing2 = new \PhpOffice\PhpSpreadsheet\Worksheet\Drawing();
+                $objDrawing2->setName('FCD');
+                $objDrawing2->setDescription('FCD');
+                $objDrawing2->setPath(public_path('frontend/images/fcd.png'));
+                $objDrawing2->setWidthAndHeight(148,74);
+                $objobjDrawing2->setResizeProportional(true);
+                $objDrawing2->setCoordinates('H2');
 
                 $drawing2->setWorksheet($event->sheet->getDelegate());
             },
