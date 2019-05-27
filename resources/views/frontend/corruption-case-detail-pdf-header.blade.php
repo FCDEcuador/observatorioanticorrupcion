48085a92328ca4c5dd080127e856aeb1fea1cb23
Pablo Reyes <yellowgamepjrr@gmail.com>
2019-05-27 16:35:55 -0500

    cambios footer pdf

--- resources/views/frontend/corruption-case-detail-pdf-header.blade.php
+++ resources/views/frontend/corruption-case-detail-pdf-header.blade.php
@@ -3,18 +3,14 @@
 		<tr>
 			<td valign="middle" align="center">
 
-	      		@if($oConfiguration->frontend_logo != '')
+	      		
 	      			<img src="{!! asset($oStorage->url($oConfiguration->frontend_logo)) !!}" width="80%" />
-	      		@else
-	      			<img src="{!! asset('public/frontend/images/logo-sitio.png') !!}" />
-	      		@endif
+	      		
 			</td>
 			<td valign="middle" align="center">
-				@if($oConfiguration->backend_logo != '')
+				
                     <img src="{!! asset($oStorage->url($oConfiguration->backend_logo)) !!}" />
-                @else
-                    <img src="{!! asset('public/frontend/images/fcd.png') !!}" class="pl-3" />
-                @endif
+                
 			</td>
 		</tr>
 	</table>
